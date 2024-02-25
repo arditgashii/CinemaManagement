1b88a7040fde3bdc09b9a4f63f6fac4e911090f9
gashiardit <arditfcb58@gmail.com>
2024-02-25 22:12:27 +0100

    Added CRUD components for Movies

--- src/app/views/movies/movies.module.ts
+++ src/app/views/movies/movies.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoviesComponent } from './movies.component';
+import { AllMoviesComponent } from './components/all-movies/all-movies.component';
+import { CreateMovieComponent } from './components/create-movie/create-movie.component';
+import { EditMovieComponent } from './components/edit-movie/edit-movie.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 
 
 @NgModule({
   declarations: [
-    MoviesComponent
+    MoviesComponent,
+    AllMoviesComponent,
+    CreateMovieComponent,
+    EditMovieComponent,
+    MovieDetailsComponent
   ],
   imports: [
     CommonModule
