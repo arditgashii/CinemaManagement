7f3b3c7717f7bd028ef8212ef97d88d481c15b7e
gashiardit <arditfcb58@gmail.com>
2024-02-25 21:36:26 +0100

    Added Views Module and Views Component files

---
+++ src/app/views/views.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewsComponent } from './views.component';
+
+
+
+@NgModule({
+  declarations: [
+    ViewsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ViewsModule { }
